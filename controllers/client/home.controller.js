ca8fc15490320f69d78f6c7eb98fa0b2e0cbacc4
Hair Salon Developer <developer@hairsalon.com>
2025-10-29 15:54:59 +0700

    hiển thị sản phẩm nổi bật

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,5 +1,22 @@
-module.exports.index = (req, res) => {
-  res.render('client/pages/home/index.pug', { 
-    title: 'Home'
-});
+const Product = require("../../model/products.model.js");
+
+// [GET] /
+module.exports.index = async (req, res) => {
+    const find = {
+        deleted: false,
+        status: "active",
+        featured: "1"
+    }
+
+    const products = await Product.find(find).limit(3);
+
+    const newProducts = products.map( product => {
+        product.newPrice = (product.price * (100 - product.discountPercentage) / 100).toFixed(0);
+        return product
+    })
+
+    res.render('client/pages/home/index.pug', {
+        title: 'Home',
+        products: newProducts
+    });
 }
