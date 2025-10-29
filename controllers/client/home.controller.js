2ba28074f42b4f3845c9815318216073f788261c
Hair Salon Developer <developer@hairsalon.com>
2025-10-29 16:03:55 +0700

    hiển thị danh sách sản phẩm mới

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -2,21 +2,39 @@ const Product = require("../../model/products.model.js");
 
 // [GET] /
 module.exports.index = async (req, res) => {
+
+    // Lấy anh sách sản phẩm nổi bật
     const find = {
         deleted: false,
         status: "active",
         featured: "1"
     }
 
-    const products = await Product.find(find).limit(3);
+    const productsFeatured = await Product.find(find).limit(3);
 
-    const newProducts = products.map( product => {
+    const newProductsFeatured = productsFeatured.map( product => {
         product.newPrice = (product.price * (100 - product.discountPercentage) / 100).toFixed(0);
-        return product
+        return product;
+    });
+    // Kết thúc lấy danh sách sản phẩm nổi bật
+
+
+    // Lấy anh sách sản phẩm mới
+    const productsNew = await Product.find({
+        deleted: false,
+        status: "active",
+    }).limit(6).sort({position: "desc"});
+
+    const newProductsNew = productsNew.map( product => {
+        product.newPrice = (product.price * (100 - product.discountPercentage) / 100).toFixed(0);
+        return product;
     })
+    // Kết thúc lấy anh sách sản phẩm mới
+
 
     res.render('client/pages/home/index.pug', {
         title: 'Home',
-        products: newProducts
+        productsFeatured: newProductsFeatured,
+        productsNew: newProductsNew
     });
 }
